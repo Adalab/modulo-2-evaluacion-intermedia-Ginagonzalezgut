41c1291e4f4741c9d8864c8102119532a90c7375
Gina González <gini_go@hotmail.com>
2024-07-04 10:59:19 +0200

    add javascript for game

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
 "use strict";
 
-// 1.la usuaria hace clik en el boton prueba
-// 2.Recoger el numero que ha enviado la usuaria
-// 3. IF -----si la usuario escribe un Numero > y haga click en prueba:
-// Muestre en en el parrafo es demasiado alto
-// Si la usuaria escribe un numero < y haga click en prueba,
-//  muestra  en el parrafo demasiado bajo
-// Si la usuaria introduce un numeor = que el aleatorio y hace click
-// muestra en el parrafo Has ganado campeona
-// Cuanod la jugadora no introduzca un numero valido y pulse  en prueba:
-// muestre en el parrafo el número debe estar entre 1 y 100
-
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
@@ -19,14 +8,29 @@ const button = document.querySelector(".js-button");
 const inputNumber = document.querySelector(".js-input-number");
 const randomNumber = getRandomNumber(100);
 const ParagraphClue = document.querySelector(".js-paragraph-clue");
-
-console.log(randomNumber);
+const ParagraphTries = document.querySelector(".paragraph__numberoftries");
+let acc = 0;
 
 button.addEventListener("click", (event) => {
   event.preventDefault();
-
+  console.log(randomNumber);
   const number = inputNumber.value;
+  console.log(number);
+
   if (number > randomNumber) {
     ParagraphClue.innerHTML = "Es demasiado alto";
   }
+  if (number < randomNumber) {
+    ParagraphClue.innerHTML = "Es demasiado bajo";
+  }
+  if (number == randomNumber) {
+    ParagraphClue.innerHTML = "Has ganado campeona!!!";
+  }
+  if ((number < 1, number > 100)) {
+    ParagraphClue.innerHTML = "El número debe estar entre 1 y 100";
+  }
+
+  acc += 1;
+
+  ParagraphTries.innerHTML = `número de intentos:${acc}`;
 });
